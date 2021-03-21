c049e34a635d238c6744c855ce35074f78d91786
ryan <ryan147k@gmail.com>
2021-03-21 08:57:12 +0000

    1. 更新了模型训练停止策略 2. 更新了模型保存策略 3. 添加了BiLSTM模型

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ from torch.utils.data import DataLoader
 import torchtext
 from data import train_data_snli, val_data_snli, test_data_snli
 from data import SNLIDataset, VOCAB, _collate_fn
-from model import WordAvg
+from model import WordAvg, BiLSTM
 from tqdm import tqdm
 import argparse
 import time
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', type=int, default=2048)
+parser.add_argument('--batch_size', type=int, default=512)
 parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--weight_decay', type=float, default=1e-3)
 parser.add_argument('--epoch_num', type=int, default=1000)
 
 args = parser.parse_args()
@@ -52,18 +53,19 @@ snli_test_dataloader = DataLoader(dataset=snli_test_dataset,
                                     collate_fn=_collate_fn)
 
 # model
-model = WordAvg(num_embeddings=len(VOCAB),
-                embedding_dim=100,
-                in_features=128,
-                out_features=3)
+# model = WordAvg(num_embeddings=len(VOCAB), embedding_dim=100, in_features=256, num_class=3)
+model = BiLSTM(num_embeddings=len(VOCAB), embedding_dim=100, hidden_size=256, num_class=3)
 model.embedding.weight.data.copy_(VOCAB.vectors)
 model.embedding.requires_grad_ = False
 # 多GPU运行
 model = nn.DataParallel(model)
 model = model.to(device)
+print(model.module)
 
 loss_fn = nn.CrossEntropyLoss(reduction='sum')
-optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
+                                     lr=args.lr, 
+                                     weight_decay=args.weight_decay)
 
 
 # train
@@ -92,7 +94,12 @@ def val(dataloader, val=True):
 
         s = 'val  ' if val else 'test '
         print("\t {} loss: {:.4} val_acc: {:.3}".format(s, loss, acc))
+        return acc
 
+
+best_val_acc, best_val_epoch = 0, None      # 记录全局最优信息
+best_test_acc, best_test_epoch = 0, None
+loss_last = 1e10
 
 for epoch in range(1, args.epoch_num+1):
     loss_sum = 0
@@ -119,8 +126,27 @@ for epoch in range(1, args.epoch_num+1):
     print("\nEpoch {}".format(epoch))
     print("\t correct: {} total: {}".format(correct, total))
     print("\t trian loss: {:.4} acc: {:.3}".format(loss, acc))
-    val(snli_val_dataloader)
-    val(snli_test_dataloader, val=False)
-
-    if epoch % 20 == 0:
+    val_acc = val(snli_val_dataloader, val=True)
+    test_acc = val(snli_test_dataloader, val=False)
+    
+    # 更新全局最优信息
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        best_val_epoch = epoch
+        # 保存模型
         torch.save(model.module.state_dict(), 'ckpts/{}_{}_{}.pt'.format(model.module.__class__.__name__, epoch, time.time()))
+    if test_acc > best_test_acc:
+        best_test_acc = test_acc
+        best_test_epoch = epoch
+
+    # if epoch % 20 == 0:
+    #     torch.save(model.module.state_dict(), 'ckpts/{}_{}_{}.pt'.format(model.module.__class__.__name__, epoch, time.time()))
+
+    # loss不下降则停止训练
+    if loss_last - loss < 1e-6:
+        break
+    loss_last = loss    # 更新loss_last
+
+print('\n')
+print("Best Val Acc: {:.3} Epoch: {}".format(best_val_acc, best_val_epoch))
+print("Best Test Acc: {:.3} Epoch: {}".format(best_test_acc, best_test_epoch))
--- model.py
+++ model.py
@@ -15,28 +15,60 @@ class WordAvg(nn.Module):
     sequence每个token的embedding求平均
     送到MLP输出分类结果
     """
-    def __init__(self, num_embeddings, embedding_dim, in_features, out_features):
+    def __init__(self, num_embeddings, embedding_dim, in_features, num_class):
         super(WordAvg, self).__init__()
 
         self.embedding = nn.Embedding(num_embeddings=num_embeddings,
                                         embedding_dim=embedding_dim)
-        self.linear = nn.Linear(in_features=embedding_dim,
-                                out_features=in_features)
+
+        self.linear = nn.Sequential(nn.Linear(embedding_dim*2, in_features),
+                                   nn.ReLU(),
+                                    nn.Linear(in_features, in_features),
+                                    nn.ReLU(),
+                                    nn.Linear(in_features, in_features),
+                                    nn.ReLU())
+
         self.predict = nn.Linear(in_features=in_features,
-                                    out_features=out_features)
+                                    out_features=num_class)
 
     def forward(self, premise, hypothesis):
-        x = torch.cat((premise, hypothesis), dim=-1)
-        embedding = self.embedding(x)
-        x = torch.mean(embedding, dim=1)
-        x = F.relu(self.linear(x))
+        premise_emb = torch.mean(self.embedding(premise), dim=1)
+        hypothesis_emb = torch.mean(self.embedding(hypothesis), dim=1)
+        x = torch.cat((premise_emb, hypothesis_emb), dim=-1)
+        x = self.linear(x)
         x = self.predict(x)
         return x
 
 
 class BiLSTM(nn.Module):
-    def __init__(self, num_embeddings, embedding_dim):
+    def __init__(self, num_embeddings, embedding_dim, hidden_size, num_class):
         super(BiLSTM, self).__init__()
 
         self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+        self.bilstm = nn.LSTM(input_size=embedding_dim, 
+                                hidden_size=hidden_size,
+                                num_layers=2,
+                                batch_first=True,
+                                bidirectional=True)
         
+        self.linear = nn.Sequential(nn.Linear(hidden_size*2*2, hidden_size),
+                                    nn.ReLU(),
+                                    nn.Linear(hidden_size, hidden_size),
+                                    nn.ReLU(),
+                                    nn.Linear(hidden_size, hidden_size),
+                                    nn.ReLU())
+        
+        self.predict = nn.Linear(hidden_size, num_class)
+    
+    def forward(self, premise, hypothesis):
+        premise_emb = self.embedding(premise)
+        hypothesis_emb = self.embedding(hypothesis)
+        p_h, _ = self.bilstm(premise_emb)
+        h_h, _ = self.bilstm(hypothesis_emb)
+        p_h = torch.mean(p_h, dim=1)
+        h_h = torch.mean(h_h, dim=1)
+        
+        x = torch.cat((p_h, h_h), dim=-1)
+        x = self.linear(x)
+        x = self.predict(x)
+        return x
